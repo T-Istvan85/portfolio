364e5cb5ee0bdb38cf13bb38b21b016c0c709d87
airbourne85 <airbourne85@gmail.com>
2023-01-19 01:49:38 +0100

    menü javascript hozzáadás + ismeretek rész

---
+++ src/main.js
@@ -0,0 +1,13 @@
+let elem = document.getElementById("navli");
+let elemchild = elem.querySelectorAll('.hamburger');
+const checkbox = document.getElementById("nav-checkbox");
+for(i=0; i < elemchild.length ; i++){
+elemchild[i].addEventListener("click", function () {
+    
+    if(checkbox.checked === true){
+checkbox.checked = false;
+    }
+  
+})};
+
+
