1781075e952ee5a7a280882ed779dcf86cf5176c
airbourne85 <airbourne85@gmail.com>
2023-01-28 17:20:38 +0100

    kapcsolat rész és javascript fejlesztés

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 let elem = document.getElementById("navli");
-let elemchild = elem.querySelectorAll('.hamburger');
+let elemchild = elem.querySelectorAll(".hamburger");
 const checkbox = document.getElementById("nav-checkbox");
-for(i=0; i < elemchild.length ; i++){
-elemchild[i].addEventListener("click", function () {
-    
-    if(checkbox.checked === true){
-checkbox.checked = false;
+for (i = 0; i < elemchild.length; i++) {
+  elemchild[i].addEventListener("click", function () {
+    if (checkbox.checked === true) {
+      checkbox.checked = false;
     }
-  
-})};
-
-
+  });
+}
